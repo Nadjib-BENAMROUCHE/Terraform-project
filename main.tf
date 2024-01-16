05c04be2f2afc9117665b6b31d08d5b114f892dc
Amar-Dgb <amar.he@kuikops.com>
2024-01-16 17:02:55 +0100

    Add first code (Ec2, Subnet, internet_Gatway, ssh_connextion, route_table, data_vpc)

--- main.tf
+++ main.tf
@@ -36,9 +36,9 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-data "aws_vpcs" "existing_vpc" {                             # or aws_vpc 
+data "aws_vpc" "vpc_terraform" {                             # or aws_vpc 
   # Récupérer le VPC en fonction de l'ID
-  # ids = ["vpc-005e78293bf3c2b77"]
+  # ids = ["vpc-005e78293bf3c2b77"]                          # ça ne marche pas
   # OU récupérer le VPC en fonction du nom (par exemple, "mon-vpc")
   filter {
     name   = "tag:Name"
@@ -47,34 +47,34 @@ data "aws_vpcs" "existing_vpc" {                             # or aws_vpc
 }
 
 output "existing_vpc_id" {
-  value = data.aws_vpcs.existing_vpc.ids[0]
+  value = data.aws_vpc.vpc_terraform.id
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "test_terraform_subnet_public_amar_henni" {
   # vpc_id     = "vpc-005e78293bf3c2b77"
-  vpc_id     = data.aws_vpcs.existing_vpc.ids[0]
+  vpc_id     = data.aws_vpc.vpc_terraform.id
   cidr_block = "50.10.190.0/24"
   tags = {
-    Name = "subnet test nadjib 001"
+    Name = "subnet_terraform_amar_henni"
   }
 }
 
-data "aws_internet_gateway" "default" {
+data "aws_internet_gateway" "gateway" {
   filter {
     name   = "attachment.vpc-id"
-    values = [data.aws_vpcs.existing_vpc.ids[0]]
+    values = [data.aws_vpc.vpc_terraform.id]
   }
 }
 
 output "internet_gateway_id" {
-  value = data.aws_internet_gateway.default.id
+  value = data.aws_internet_gateway.gateway.id
 }
 
-resource "aws_instance" "ec2-test" {
+resource "aws_instance" "ec2-amar" {
     ami = "ami-0905a3c97561e0b69"
     instance_type = "t2.micro"
-    count = 1
-    subnet_id = aws_subnet.main.id
+    #count = 1
+    subnet_id = aws_subnet.test_terraform_subnet_public_amar_henni.id
     #key_name = "shittest"
     tags = {
       Name = "Test-EC2-terraform-001"
@@ -87,7 +87,7 @@ resource "aws_instance" "ec2-test" {
 }
 
 data "aws_eip" "by_public_eip" {
-  public_ip = "54.77.91.63"
+  public_ip = "54.154.167.140"
 }
 
 output "eip_id" {
@@ -95,26 +95,26 @@ output "eip_id" {
 }
 
 #Table de routage pour la subnet
-resource "aws_route_table" "public_route_table_nadjib_001" {
-    vpc_id = data.aws_vpcs.existing_vpc.ids[0]
+resource "aws_route_table" "public_route_table_amar" {
+    vpc_id = data.aws_vpc.vpc_terraform.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = data.aws_internet_gateway.default.id
+        gateway_id = data.aws_internet_gateway.gateway.id
     }
     tags = {
-        Name = "public_route_table_nadjib-test-001"
+        Name = "public_route_table_amar"
     }
 }
 
 resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.ec2-test[0].id
+  instance_id   = aws_instance.ec2-amar.id
   allocation_id = data.aws_eip.by_public_eip.id
 }
 
 #Association de la table de routage pb au subnet public
 resource "aws_route_table_association" "public_subnet_association" {
-    subnet_id      = aws_subnet.main.id
-    route_table_id = aws_route_table.public_route_table_nadjib_001.id
+    subnet_id      = aws_subnet.test_terraform_subnet_public_amar_henni.id
+    route_table_id = aws_route_table.public_route_table_amar.id
 }
 
 
